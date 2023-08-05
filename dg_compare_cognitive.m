11ce37bafe5b30b446be15971a6f6f2914e5d17b
davidrg70 <126163981+davidrg70@users.noreply.github.com>
2023-08-05 16:23:59 +0200

    Add files via upload

---
+++ dg_compare_cognitive.m
@@ -0,0 +1,43 @@
+% PRs to Z-scores converter (import using .txt files only) Created by David Garnica
+% david.garnica@med.uni-goettingen.de
+% 2022, Universitätsmedizin Göttingen, Neurology Department
+
+% dg_compare_cognitive
+
+% load z-scores
+clear all;
+analysis_dir = '/home/uni10/nmri/projects/dgarnica/MRI_EEG_PROSPECTIVE/Rolandic_EEG';
+load(fullfile(analysis_dir, 'Z_scores_patients_controls.mat'));
+
+subtests = {'Data','TIQ','SV','VRV','FS','AGD','VG','WLT','Lesefehler','TA','Lesefehler_TA','Woerter'};
+
+% determine number of patients and controls
+for i = 1:length(subjects)
+    if contains(subjects(i), 'P.')
+        patients_num = i;
+    end
+end
+controls_num = length(subjects) - patients_num;
+
+% separate data into groups
+patients = z(1:patients_num,:);
+controls = z(patients_num+1:end,:);
+
+% remove NaNs (that is, children that were not tested)
+patients(any(isnan(patients), 2), :) = [];
+controls(any(isnan(controls), 2), :) = [];
+
+% compare, using Wilcoxon rank sum test (equivalent to the Mann-Whitney U-test)
+for i = 1:length(subtests)-1
+    [p(i),h(i)] = ranksum(patients(:,i), controls(:,i));
+    Mp(i) = median(patients(:,i));
+    Mc(i) = median(controls(:,i));
+end
+
+% OPTIONAL: apply FDR correction for multiple comparisons
+[h, crit_p, ~, adj_p] = fdr_bh(p, 0.05, 'pdep', 'yes');
+
+results = cat(1, adj_p, h, Mc, Mp);
+what = ["p(FDR)"; "h"; "Median patients"; "Median controls"];
+results = cat(2, what, results);
+results = array2table(results, "VariableNames", string(subtests));
